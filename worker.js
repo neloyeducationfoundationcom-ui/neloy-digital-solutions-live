37334c35165537b32684f64ae5314f18d9865761
neloyeducationfoundationcom-ui <neloy.educationfoundation.com@gmail.com>
2026-09-08 23:29:05 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,9 @@
-
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("Neloy Digital Solutions is working!", {
+      headers: {
+        "content-type": "text/plain; charset=UTF-8"
+      }
+    });
+  }
+};
